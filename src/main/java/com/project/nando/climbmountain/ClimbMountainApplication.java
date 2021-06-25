744d55252d1d92ccf14ee1b9dfd05e32313fc413
Suhartono <suhartonoyang@gmail.com>
2021-06-25 15:03:45 +0700

    inital project

---
+++ src/main/java/com/project/nando/climbmountain/ClimbMountainApplication.java
@@ -0,0 +1,13 @@
+package com.project.nando.climbmountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbMountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbMountainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nando/climbmountain/ClimbMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nando.climbmountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbMountainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
